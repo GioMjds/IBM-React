d7be2df8fc92029778cb8a783f993362112a686f
GioMjds <giomjds@gmail.com>
2025-01-25 10:00:21 +0800

    Init project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,20 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, description, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) existingItem.quantity += 1;
+      else state.items.push({ name, image, description, cost, quantity: 1 });
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      state.items = state.items.filter((item) => item.name !== name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, amount } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) itemToUpdate.quantity = amount;
     },
   },
 });
